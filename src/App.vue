756a210410fe91c1495d066de31c9117cc82341d
Indrashish Ghosh <mail@indrashishghosh.com>
2017-03-09 18:30:21 +0530

    Init 2.0

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <gradient></gradient>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+import Gradient from './components/Gradient';
+
+export default {
+  name: 'app',
+  components: {
+    Gradient,
+    Hello,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Ubuntu, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/Gradient.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gradient">
+    <h1>An awesome gradient</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gradient',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  font-size: 16px;
+  color: #36c;
+}
+</style>

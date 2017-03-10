e257923810dd2db82a8d205cdd6418dbe244a109
Indrashish Ghosh <mail@indrashishghosh.com>
2017-03-10 12:45:10 +0530

    Added social links

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -5,11 +5,22 @@
       <p>Aquatic Blue</p>
     </div>
 
+    <ul class="nav" id="nav">
+      <li class="nav__item" id="nav--prev"><LeftArrow class="nav__arrow nav__arrow--left"/></li>
+      <li class="nav__item" id="nav--next"><RightArrow class="nav__arrow nav__arrow--right"/></li>
+    </ul>
+
   </main>
 </template>
 
 <script>
+import LeftArrow from '../assets/leftchev.svg';
+import RightArrow from '../assets/rightchev.svg';
+
 export default {
   name: 'display',
+  components: {
+    LeftArrow, RightArrow,
+  },
 };
 </script>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,19 +2,40 @@
   <header class="header">
     <Logo class="header__logo" />
     <ul class="social header__social">
-      <li>Twitter</li>
-      <li>Facebook</li>
+      <li>
+        <a href="" class="button button--twitter">
+          <Twitter class="button__icon" />
+          <span>Share on Twitter</span>
+        </a>
+      </li>
+      <li class="ml10">
+        <a href="" class="button button--facebook">
+          <Facebook class="button__icon" />
+          <span>Share on Facebook</span>
+        </a>
+      </li>
+      <li class="ml10">
+        <a href="">
+          <Github class="header__github" />
+        </a>
+      </li>
     </ul>
   </header>
 </template>
 
 <script>
 import Logo from '../assets/logo.svg';
+import Facebook from '../assets/facebook.svg';
+import Twitter from '../assets/twitter.svg';
+import Github from '../assets/github.svg';
 
 export default {
   name: 'topbar',
   components: {
     Logo,
+    Facebook,
+    Twitter,
+    Github,
   },
   data() {
     return {

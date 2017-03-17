d454b0b217562d4a08e3006cb02973d6ad5f6e1f
Indrashish Ghosh <mail@indrashishghosh.com>
2017-03-17 19:18:58 +0530

    Showing success message on copy

--- src/components/CodeModal.vue
+++ src/components/CodeModal.vue
@@ -1,17 +1,25 @@
 <template>
-  <Modal :show.sync="show" :on-close="closeModals">
+  <Modal :show.sync="show" :on-close="closeModal">
 
       <h3 class="modal__title">
-        Copy CSS Code
+        Copy CSS code
       </h3>
 
-<pre class="codeblock"><code>
-    <span class="codeblock__property">background</span>: <span class="codeblock__spec">{{ this.gradient.colors[0] }}</span>; <span class="codeblock__comment">/* fallback for old browsers */</span>
-    <span class="codeblock__property">background</span>: -webkit-linear-gradient(to left, <span class="codeblock__spec">{{ [...this.gradient.colors].join(', ') }}</span>); <span class="codeblock__comment">/* Chrome 10-25, Safari 5.1-6 */</span>
-    <span class="codeblock__property">background</span>: linear-gradient(to left, <span class="codeblock__spec">{{ [...this.gradient.colors].join(', ') }}</span>); <span class="codeblock__comment">/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */</span>
+<div class="modal__content">
+
+    <div class="modal__success" v-if="copied">
+      <p>{{this.showSuccessMessage()}}</p>
+    </div>
+
+<pre class="codeblock" v-else><code>
+  <span class="codeblock__property">background</span>: <span class="codeblock__spec">{{ this.gradient.colors[0] | lowercase }}</span>; <span class="codeblock__comment">/* fallback for old browsers */</span>
+  <span class="codeblock__property">background</span>: -webkit-linear-gradient(to left, <span class="codeblock__spec">{{ [...this.gradient.colors].join(', ') | lowercase }}</span>); <span class="codeblock__comment">/* Chrome 10-25, Safari 5.1-6 */</span>
+  <span class="codeblock__property">background</span>: linear-gradient(to left, <span class="codeblock__spec">{{ [...this.gradient.colors].join(', ') | lowercase }}</span>); <span class="codeblock__comment">/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */</span>
 </code></pre>
 
-      <button class="btn">
+</div>
+
+      <button class="btn" v-clipboard="copyData" @success="handleSuccess" @error="handleError">
           Copy Code
       </button>
 
@@ -27,6 +35,51 @@ export default {
   props: ['show', 'gradient', 'closeModals'],
   components: {
     Modal,
+  },
+  data() {
+    return {
+      copied: false,
+    };
+  },
+  filters: {
+    lowercase(value) {
+      return (!value) ? '' : value.toString().toLowerCase();
+    },
+  },
+  computed: {
+    copyData() {
+      return `background: ${this.gradient.colors[0]};  /* fallback for old browsers */
+-webkit-linear-gradient(to left, ${[...this.gradient.colors].join(', ')});  /* Chrome 10-25, Safari 5.1-6 */
+linear-gradient(to left, ${[...this.gradient.colors].join(', ')}); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+    },
+  },
+  methods: {
+    closeModal() {
+      this.closeModals();
+      this.copied = false;
+    },
+    showSuccessMessage() {
+      const messages = [
+        '👊 You got it! Go make something awesome.',
+        '👌 Excellent Choice! It\'ll look amazing.',
+        '🙌 Very well, master.',
+        '👍 Done. Go for it.',
+        '✌️ Nice one! You have good tase.',
+      ];
+      const length = messages.length;
+      const randomMessage = Math.round(Math.random() * (length - 1));
+      return messages[randomMessage];
+    },
+    handleSuccess() {
+      this.copied = true;
+    },
+    handleError(e) {
+      console.log(e);
+    },
+  },
+  updated() {
+    this.copied = false;
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueClipboards from 'vue-clipboards';
 import App from './App';
 
 require('./stylesheets/app.scss');
+
+Vue.use(VueClipboards);
 
 Vue.config.productionTip = false;
 

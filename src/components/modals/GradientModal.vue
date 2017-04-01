98aad15af8735180fd8dea60b4ae734e15013995
Indrashish Ghosh <mail@indrashishghosh.com>
2017-04-01 22:51:51 +0530

    Add contributing url

--- src/components/modals/GradientModal.vue
+++ src/components/modals/GradientModal.vue
@@ -9,9 +9,9 @@
         Adding a gradient is easy. All gradients are read from a gradients.json file which is available in this project's repo. Simply add your gradient details to it and submit a pull request.
       </p>
 
-      <button class="btn">
+      <a class="btn" href="https://github.com/Ghosh/uiGradients#contributing" target="_blank">
           Tell me more
-      </button>
+      </a>
 
   </Modal>
 </template>

5632795d170e187225284ba09588d676c92badf6
Indrashish Ghosh <mail@indrashishghosh.com>
2017-04-01 23:20:25 +0530

    Added social links

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,13 +3,14 @@
     <Logo class="header__logo" />
     <ul class="social header__social">
       <li>
-        <a href="" class="button button--twitter">
+        <a
+          href="#" class="button button--twitter" @click.prevent="shareOnTwitter()">
           <Twitter class="button__icon" />
           <span>Share on Twitter</span>
         </a>
       </li>
       <li class="ml10">
-        <a href="" class="button button--facebook">
+        <a href="#" class="button button--facebook" @click.prevent="shareOnFacebook()">
           <Facebook class="button__icon" />
           <span>Share on Facebook</span>
         </a>
@@ -36,6 +37,20 @@ export default {
     Facebook,
     Twitter,
     Github,
+  },
+  methods: {
+    shareOnTwitter() {
+      window.open(
+        'http://twitter.com/share?url=http://uigradients.com&amp;text=Beautiful%20color%20gradients%20for%20designers%20and%20developers&amp;via=_ighosh',
+        'twitter-share',
+        'width=700,height=300');
+    },
+    shareOnFacebook() {
+      window.open(
+        'http://www.facebook.com/sharer.php?u=http://uigradients.com',
+        'facebook-share',
+        'width=700,height=400');
+    },
   },
 };
 </script>

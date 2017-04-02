5f48cd99764cdf64df8889df1f8878d9643969d2
Indrashish Ghosh <mail@indrashishghosh.com>
2017-04-03 02:56:40 +0530

    Cosmetic changes

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ Vue.use(VueClipboards);
 Vue.use(VueAnalytics, {
   id: 'UA-49604576-2',
   autoTracking: false,
-  debug: {
-    enabled: true,
-    trace: false,
-    sendHitTask: true,
-  },
+  // debug: {
+  //   enabled: true,
+  //   trace: false,
+  //   sendHitTask: true,
+  // },
 });
 
 Vue.config.productionTip = false;
@@ -25,3 +25,11 @@ new Vue({
   template: '<App/>',
   components: { App },
 });
+
+/* eslint-disable no-console */
+/* eslint-disable no-useless-escape */
+
+console.log('uigradients.com - Beautiful color gradients for design and code');
+console.log('----------------------------------------------------------------');
+console.log('Have a side project you want to collab on? Get in touch!');
+console.log('I am at - https://twitter.com/_ighosh');
